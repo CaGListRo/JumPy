b883817841850cac1d17c6746dfedc54644e1c22
CaGListRo <paco.nyns@gmail.com>
2024-12-06 11:09:42 +0100

    Added the last missing type hints, return type hints and doc strings.

--- settings.py
+++ settings.py
@@ -1,12 +1,15 @@
-MAIN_WINDOW_RESOLUTION = (1400, 900)
-GAME_WINDOW_RESOLUTION = (700, 850)
-FRAME_THICKNESS = 5
-WINDOW_FRAME_POSITION = (MAIN_WINDOW_RESOLUTION[0] // 2 - GAME_WINDOW_RESOLUTION[0] // 2 - FRAME_THICKNESS, MAIN_WINDOW_RESOLUTION[1] // 2 - GAME_WINDOW_RESOLUTION[1] // 2 - FRAME_THICKNESS)
-WINDOW_FRAME_SIZE = (GAME_WINDOW_RESOLUTION[0] + 2 * FRAME_THICKNESS, GAME_WINDOW_RESOLUTION[1] + 2 * FRAME_THICKNESS)
+from typing import Final
 
-BLACK = (1, 1, 1)
+MAIN_WINDOW_RESOLUTION: Final[tuple[int]] = (1400, 900)
+GAME_WINDOW_RESOLUTION: Final[tuple[int]] = (700, 850)
+FRAME_THICKNESS: Final[int] = 5
+WINDOW_FRAME_POSITION: Final[tuple[int]] = (int(MAIN_WINDOW_RESOLUTION[0] // 2 - GAME_WINDOW_RESOLUTION[0] // 2 - FRAME_THICKNESS),
+                                            int(MAIN_WINDOW_RESOLUTION[1] // 2 - GAME_WINDOW_RESOLUTION[1] // 2 - FRAME_THICKNESS))
+WINDOW_FRAME_SIZE: Final[tuple[int]] = (GAME_WINDOW_RESOLUTION[0] + 2 * FRAME_THICKNESS, GAME_WINDOW_RESOLUTION[1] + 2 * FRAME_THICKNESS)
 
-BUTTON_COLORS = {
+BLACK: Final[tuple[int]] = (1, 1, 1)
+
+BUTTON_COLORS: Final[dict[dict[tuple[int]]]] = {
     'green': {'color': (56, 155, 60), 'hover_color': (76, 175, 80), 'shadow_color': (16, 115, 20), 'frame_color': (6, 95, 20)},
     'yellow': {'color': (235, 235, 0), 'hover_color': (255, 255, 50), 'shadow_color': (195, 195, 0), 'frame_color': (125, 125, 0)},
     'red': {'color': (235, 0, 0), 'hover_color': (255, 50, 50), 'shadow_color': (175, 0, 0), 'frame_color': (100, 0, 0)}
--- utilities.py
+++ utilities.py
@@ -1,5 +1,12 @@
 import pygame as pg
 
-def load_image(imagename):
-    img = pg.image.load('images/' + imagename + '.png').convert_alpha()
+def load_image(imagename: str) -> pg.Surface:
+    """
+    Load an image from a file.
+    Args:
+    imagename (str): The name of the image. (Without ".png")
+    Returns:
+    pg.Surface: The loaded image.
+    """
+    img: pg.Surface = pg.image.load('images/' + imagename + '.png').convert_alpha()
     return img
